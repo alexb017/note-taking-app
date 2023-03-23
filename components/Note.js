78b2061afa62138f8d38ecd5ab2fd2416fa80ba8
alexb017 <24658786+alexb017@users.noreply.github.com>
2023-03-23 22:35:31 +0200

    commit

--- components/Note.js
+++ components/Note.js
@@ -41,6 +41,12 @@ export default function Note(props) {
             <div onClick={openModal}>
                 {details.title && <div className={styles.noteTitle} aria-multiline="true" role="textbox">{details.title}</div>}
                 <div className={styles.noteContent} aria-multiline="true" role="textbox">{details.content}</div>
+                {details.date || details.time ?
+                    <div className={styles.dateTimeContent}>
+                        <p className={styles.dateTime}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 256 256"><path d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24Zm0 192a88 88 0 1 1 88-88a88.1 88.1 0 0 1-88 88Zm64-88a8 8 0 0 1-8 8h-56a8 8 0 0 1-8-8V72a8 8 0 0 1 16 0v48h48a8 8 0 0 1 8 8Z" /></svg>
+                            {`${details.date} ${details.time}`}</p>
+                    </div> : ""}
             </div>
 
             {isModalOpen && <NoteModal onIsModalClose={() => setIsModalOpen(false)} />}
@@ -52,6 +58,7 @@ export default function Note(props) {
                 (<div className={styles.modalSettings} ref={modalRef}>
                     <button type="button" className={styles.modalBtn}>Delete note</button>
                     <button type="button" className={styles.modalBtn}>Add label</button>
+                    <button type="button" className={styles.modalBtn}>Pin note</button>
                 </div>)}
         </div>
     )
---
+++ components/Reminder.js
@@ -0,0 +1,9 @@
+import styles from './Reminder.module.css';
+
+export default function Reminder() {
+    return (
+        <div>
+            <p>Reminder</p>
+        </div>
+    )
+}
