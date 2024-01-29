be631a28ab688d7fe1d6f9f81bc34973168a275c
Alex <alexbacirea@gmail.com>
2024-01-29 03:37:27 +0200

    Adds new dependencies.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { nextui } from '@nextui-org/react';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,9 +9,13 @@ module.exports = {
 
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // ...
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [nextui()],
 };
